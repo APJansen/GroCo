1e8cdf0cee8ed7599ca97c70a44ec2908066a253
Aron <aronpjansen@gmail.com>
2021-09-03 12:55:37 +0200

    optimized by computing and storing once and for all a list of indices that can be used to perform the group action on the kernel just by gathering those indices.

--- groco/layers.py
+++ groco/layers.py
@@ -1,8 +1,8 @@
-from tensorflow.keras.layers import Conv2D, Input
+from tensorflow.keras.layers import Conv2D
 import tensorflow as tf
 from functools import partial
-import groups
-from utils import EquivariantPadding
+from groco import groups
+from groco.utils import EquivariantPadding
 
 
 class GroupConv2D(Conv2D):
@@ -22,14 +22,19 @@ class GroupConv2D(Conv2D):
     """
 
     def __init__(self, group, kernel_size, allow_non_equivariance: bool=False, **kwargs):
-        self.group = group if isinstance(group, groups.WallpaperGroup) else groups.group_dict(group)
-        self.group_valued_input = None  # set during build
+        self.group = group if isinstance(group, groups.WallpaperGroup) else groups.group_dict[group]
 
-        self.equivariant_padding = EquivariantPadding(allow_non_equivariance=allow_non_equivariance, kernel_size=kernel_size, **kwargs)
+        self.equivariant_padding = EquivariantPadding(allow_non_equivariance=allow_non_equivariance,
+                                                      kernel_size=kernel_size, **kwargs)
         if 'padding' in kwargs and kwargs['padding'].endswith('_equiv'):
             kwargs['padding'] = kwargs['padding'][:-6]
 
         super().__init__(kernel_size=kernel_size, **kwargs)
+
+        # set during build
+        self.group_valued_input = None
+        self.repeated_bias_indices = None
+        self.transformed_kernel_indices = None
 
     def call(self, inputs):
         group_merged_inputs = self._merge_group_axis(inputs)
@@ -37,30 +42,10 @@ class GroupConv2D(Conv2D):
 
         outputs = self._conv2d_call(
             padded_group_merged_inputs,
-            self._group_transform_kernel(),
-            self._repeat_bias()
+            self._transformed_kernel(),
+            self._repeated_bias()
         )
         return self._restore_group_axis(outputs)
-
-    def build(self, input_shape):
-        """
-        Check if the input is a signal on the group (rather than just on the grid) and stores that in attribute group_valued_input.
-        If so, merge the group axis with the channel axis.
-        Then run the parent class's build.
-        Finally run the EquivariantPadding layer's build on the merged input.
-        """
-        self.group_valued_input = len(input_shape) == 5  # this includes the batch dimension
-        if self.group_valued_input:
-            (batch, height, width, group_order, channels) = input_shape
-            assert group_order == self.group.order, f'Got input shape {input_shape}, expected {(batch, height, width, self.group.order, channels)}.'
-            input_shape = (batch, height, width, channels * group_order)
-
-        super().build(input_shape)
-
-        if self.group_valued_input:
-            self.input_spec.axes = {self._get_channel_axis(): channels}
-
-        self.equivariant_padding.build(input_shape)
 
     def _merge_group_axis(self, inputs):
         """
@@ -75,59 +60,65 @@ class GroupConv2D(Conv2D):
         batch = -1 if batch is None else batch
         return tf.reshape(inputs, (batch, height, width, group_order * channels))
 
-    def _group_transform_kernel(self):
-        """
-        Action of the whole group on the kernel.
-        Add a group axis indicating which group element was used.
+    def _transformed_kernel(self):
+        """Perform the group action on the kernel, as a signal on the group, or on the grid in a lifting convolution."""
+        return tf.gather(tf.reshape(self.kernel, [-1]), indices=self.transformed_kernel_indices, axis=0)
 
-        (height, width, channels_in, channels_out) -> (height, width, group_order, channels_in, channels_out)
+    def _repeated_bias(self):
+        """Transform the bias to group.order repeated copies of itself."""
+        return tf.gather(self.bias, indices=self.repeated_bias_indices, axis=0)
+
+    def _restore_group_axis(self, outputs):
         """
-        transformed_kernel = self.group.action(self.kernel)
+        Reshape the output of the convolution, splitting off the group index from the channel axis.
+
+        (batch, height, width, group_order * channels) -> (batch, height, width, group_order, channels)
+        """
+        batch, height, width, channels = outputs.shape
+        batch = -1 if batch is None else batch
+        return tf.reshape(outputs, (batch, height, width, self.group.order, channels // self.group.order))
+
+    def build(self, input_shape):
+        """
+        Check if the input is a signal on the group (rather than just on the grid),
+        and stores that in attribute group_valued_input.
+        If so, merge the group axis with the channel axis.
+        Then run the parent class's build.
+        Run the EquivariantPadding layer's build on the merged input.
+        Construct repeated_bias_indices and transformed_kernel_indices
+        """
+        self.group_valued_input = len(input_shape) == 5  # this includes the batch dimension
         if self.group_valued_input:
-            transformed_kernel = self._discrete_group_transform(transformed_kernel)
-
-        transformed_kernel = self._merge_group_channels_out(transformed_kernel)
-        return transformed_kernel
-
-    def group_transform(self, signal, group_element=None):
-        """
-        Act with the whole group on the input signal, which can be a signal on the domain or the group.
-        Used only as a helper method to check equivariance, not internally.
-
-        :param signal: Tensor of shape (batch, height, width, group_order, channels), group_order possibly omitted.
-        :param group_element: Index of group element to act with. Defaults to None meaning all group elements will be used.
-        :return: transformed tensor of shape (group_order, batch, height, width, group_order, channels).
-        The first axis indicates which element was acted with, and is omitted if a group_element is specified.
-        """
-        group_valued_signal = signal.shape.rank == 5
-
-        # reshape to match the kernel
-        if group_valued_signal:
-            (batch, height, width, group_order, channels) = signal.shape
+            (batch, height, width, group_order, channels) = input_shape
             assert group_order == self.group.order, f'Got input shape {input_shape}, expected {(batch, height, width, self.group.order, channels)}.'
-            transformed_signal = tf.transpose(signal, (1, 2, 3, 4, 0))
-            transformed_signal = tf.reshape(transformed_signal, (height, width, group_order * channels, batch))
-        else:
-            transformed_signal = tf.transpose(signal, (1, 2, 3, 0))
-        # shape now (height, width, channels, batch)
+            input_shape = (batch, height, width, channels * group_order)
 
-        transformed_signal = self.group.action(transformed_signal)
-        # shape now (height, width, group_order, channel, batch)
+        super().build(input_shape)
 
-        if group_valued_signal:
-            transformed_signal = self._discrete_group_transform(transformed_signal)
+        if self.group_valued_input:
+            self.input_spec.axes = {self._get_channel_axis(): channels}
 
-        transformed_signal = tf.transpose(transformed_signal, (2, 4, 0, 1, 3))
-        # shape now (group_order, batch, height, width, channel)
-        if group_valued_signal:
-          transformed_signal = tf.reshape(transformed_signal, (group_order, batch, height, width, group_order, channels))
+        self.equivariant_padding.build(input_shape)
 
-        if group_element is not None:
-          transformed_signal = transformed_signal[group_element]
+        self.repeated_bias_indices = self._compute_repeated_bias_indices()
+        self.transformed_kernel_indices = self._compute_transformed_kernel_indices()
 
-        return transformed_signal
+    def _compute_repeated_bias_indices(self):
+        """Compute a 1D tensor of indices used to gather from the bias in order to repeat it across the group axis."""
+        indices = tf.range(tf.size(self.bias))
+        indices = tf.concat([indices for _ in range(self.group.order)], axis=0)
+        return indices
 
-    def _discrete_group_transform(self, kernel):
+    def _compute_transformed_kernel_indices(self):
+        """Compute a tensor of indices used to gather from the kernel to produce the group action on it."""
+        indices = tf.reshape(tf.range(tf.size(self.kernel)), self.kernel.shape)
+        indices = self.group.action(indices)
+        if self.group_valued_input:
+            indices = self._permute_group_axis(indices)
+        indices = self._merge_group_channels_out(indices)
+        return indices
+
+    def _permute_group_axis(self, kernel):
         """
         After the action on the spatial part has been done, perform the action on the group argument.
         Permute the input group axis according to the group composition.
@@ -152,19 +143,43 @@ class GroupConv2D(Conv2D):
         transformed_kernel = tf.transpose(kernel, (0, 1, 3, 2, 4))
         return tf.reshape(transformed_kernel, (height, width, channels_in, group_order * channels_out))
 
-    def _repeat_bias(self):
-        """Transform the bias to group_order repeated copies of itself."""
-        return tf.concat([self.bias for _ in range(self.group.order)], axis=0) if self.use_bias else None
-
-    def _restore_group_axis(self, outputs):
+    def group_transform(self, signal, group_element=None):
         """
-        Reshape the output of the convolution, splitting off the group index from the channel axis.
+        Act with the whole group on a signal, which can be a signal on the domain or the group.
+        Used only as a helper method to check equivariance, not internally.
 
-        (batch, height, width, group_order * channels) -> (batch, height, width, group_order, channels)
+        :param signal: Tensor of shape (batch, height, width, group_order, channels), group_order possibly omitted.
+        :param group_element: Index of group element to act with. Defaults to None meaning all group elements are used.
+        :return: transformed tensor of shape (group_order, batch, height, width, group_order, channels).
+        The first axis indicates which element was acted with, and is omitted if a group_element is specified.
         """
-        batch, height, width, channels = outputs.shape
-        batch = -1 if batch is None else batch
-        return tf.reshape(outputs, (batch, height, width, self.group.order, channels // self.group.order))
+        group_valued_signal = signal.shape.rank == 5
+
+        # reshape to match the kernel
+        if group_valued_signal:
+            (batch, height, width, group_order, channels) = signal.shape
+            assert group_order == self.group.order, f'Got input shape {signal.shape}, expected {(batch, height, width, self.group.order, channels)}.'
+            transformed_signal = tf.transpose(signal, (1, 2, 3, 4, 0))
+            transformed_signal = tf.reshape(transformed_signal, (height, width, group_order * channels, batch))
+        else:
+            transformed_signal = tf.transpose(signal, (1, 2, 3, 0))
+        # shape now (height, width, channels, batch)
+
+        transformed_signal = self.group.action(transformed_signal)
+        # shape now (height, width, group_order, channel, batch)
+
+        if group_valued_signal:
+            transformed_signal = self._permute_group_axis(transformed_signal)
+
+        transformed_signal = tf.transpose(transformed_signal, (2, 4, 0, 1, 3))
+        # shape now (group_order, batch, height, width, channel)
+        if group_valued_signal:
+          transformed_signal = tf.reshape(transformed_signal, (group_order, batch, height, width, group_order, channels))
+
+        if group_element is not None:
+          transformed_signal = transformed_signal[group_element]
+
+        return transformed_signal
 
     def _conv2d_call(self, reshaped_inputs, reshaped_kernel, reshaped_bias):
         """
@@ -194,7 +209,7 @@ class GroupConv2D(Conv2D):
                     def _apply_fn(o):
                         return tf.nn.bias_add(o, reshaped_bias, data_format=self._tf_data_format)
 
-                    outputs = conv_utils.squeeze_batch_dims(
+                    outputs = tf.keras.utils.conv_utils.squeeze_batch_dims(
                         outputs, _apply_fn, inner_rank=self.rank + 1)
                 else:
                     outputs = tf.nn.bias_add(
