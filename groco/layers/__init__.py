2b56d2756b448846016b0e3d46995231c4421ff9
Aron <aronpjansen@gmail.com>
2021-09-15 17:34:08 +0200

    added 3d group pooling, passing same tests

--- groco/layers/__init__.py
+++ groco/layers/__init__.py
@@ -2,4 +2,5 @@ from .padding import *
 from .conv_base import *
 from .conv_2d import *
 from .conv_3d import *
-from .pooling import *
+from .pooling_2d import GroupMaxPooling2D, GroupAveragePooling2D
+from .pooling_3d import GroupMaxPooling3D, GroupAveragePooling3D
--- groco/layers/test_pooling.py
+++ groco/layers/test_pooling.py
@@ -1,46 +1,106 @@
 from tensorflow.test import TestCase
-from groco.groups.wallpaper_groups import wallpaper_group_dict
-from groco.layers import GroupMaxPooling2D
+from groco.groups import wallpaper_group_dict, space_group_dict
+from groco.layers import GroupMaxPooling2D, GroupMaxPooling3D
 from groco.utils import test_equivariance
 import tensorflow as tf
 
 
 class TestGroupPooling2D(TestCase):
+    def __init__(self, tests):
+        super().__init__(tests)
+        self.shape = (9, 8, 8, 3)
+        self.pooled_shape = (9, 4, 4, 3)
+        self.spatial_axes = (1, 2)
+        self.group_axis = 3
+        self.pool = GroupMaxPooling2D
+        self.group_dict = wallpaper_group_dict
 
     def test_pool_shape(self):
-        for group in wallpaper_group_dict.values():
-            signal_on_group = tf.random.normal(shape=(10, 28, 28, group.order, 3), seed=42)
-            pool_layer = GroupMaxPooling2D(group=group, pool_size=2, strides=2, padding='same_equiv')
+        for group in self.group_dict.values():
+            signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+            pool_layer = self.pool(group=group, pool_size=2, strides=2, padding='same_equiv')
             pooled_signal = pool_layer(signal_on_group)
-            self.assertEqual(pooled_signal.shape, (10, 14, 14, group.order, 3))
+            self.assertEqual(pooled_signal.shape, self.pooled_shape[:-1] + (group.order, self.shape[-1]))
 
     def test_pool_shape_subgroup(self):
-        for group in wallpaper_group_dict.values():
+        for group in self.group_dict.values():
             for subgroup_name in group.subgroup.keys():
-                subgroup = wallpaper_group_dict[subgroup_name]
-                signal_on_group = tf.random.normal(shape=(10, 28, 28, group.order, 3), seed=42)
-                pool_layer = GroupMaxPooling2D(group=group, pool_size=2, strides=2, padding='same_equiv',
-                                               subgroup=subgroup_name)
+                subgroup = self.group_dict[subgroup_name]
+                signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+                pool_layer = self.pool(
+                    group=group, pool_size=2, strides=2, padding='same_equiv', subgroup=subgroup_name)
                 pooled_signal = pool_layer(signal_on_group)
-                self.assertEqual(pooled_signal.shape, (10, 14, 14, subgroup.order, 3))
+                self.assertEqual(pooled_signal.shape, self.pooled_shape[:-1] + (subgroup.order, self.shape[-1]))
 
     def test_pool_equiv(self):
-        for group in wallpaper_group_dict.values():
-            signal_on_group = tf.random.normal(shape=(10, 28, 28, group.order, 3), seed=42)
-            pool_layer = GroupMaxPooling2D(group=group, pool_size=2, strides=2, padding='same_equiv')
-            equiv_diff = test_equivariance(pool_layer, signal_on_group, group_axis=3)
+        for group in self.group_dict.values():
+            signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+            pool_layer = self.pool(group=group, pool_size=2, strides=2, padding='same_equiv')
+            equiv_diff = test_equivariance(
+                pool_layer, signal_on_group, group_axis=self.group_axis, spatial_axes=self.spatial_axes)
 
             self.assertAllLess(equiv_diff, 1e-4)
 
     def test_pool_equiv_subgroup(self):
-        for group in wallpaper_group_dict.values():
+        for group in self.group_dict.values():
             for subgroup_name in group.subgroup.keys():
-                signal_on_group = tf.random.normal(shape=(10, 28, 28, group.order, 3), seed=42)
-                pool_layer = GroupMaxPooling2D(group=group, pool_size=2, strides=2, padding='same_equiv',
-                                               subgroup=subgroup_name)
-                equiv_diff = test_equivariance(pool_layer, signal_on_group, group_axis=3,
-                                               subgroup=subgroup_name)
+                signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+                pool_layer = self.pool(
+                    group=group, pool_size=2, strides=2, padding='same_equiv', subgroup=subgroup_name)
+                equiv_diff = test_equivariance(
+                    pool_layer, signal_on_group, group_axis=self.group_axis, subgroup=subgroup_name,
+                    spatial_axes=self.spatial_axes)
 
                 self.assertAllLess(equiv_diff, 1e-4)
+
+
+class TestGroupPooling3D(TestCase):
+    def __init__(self, tests):
+        super().__init__(tests)
+        self.shape = (9, 8, 8, 8, 3)
+        self.pooled_shape = (9, 4, 4, 4, 3)
+        self.spatial_axes = (1, 2, 3)
+        self.group_axis = 4
+        self.pool = GroupMaxPooling3D
+        self.group_dict = space_group_dict
+
+    def test_pool_shape(self):
+        for group in self.group_dict.values():
+            signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+            pool_layer = self.pool(group=group, pool_size=2, strides=2, padding='same_equiv')
+            pooled_signal = pool_layer(signal_on_group)
+            self.assertEqual(pooled_signal.shape, self.pooled_shape[:-1] + (group.order, self.shape[-1]))
+
+    def test_pool_shape_subgroup(self):
+        for group in self.group_dict.values():
+            for subgroup_name in group.subgroup.keys():
+                subgroup = self.group_dict[subgroup_name]
+                signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+                pool_layer = self.pool(
+                    group=group, pool_size=2, strides=2, padding='same_equiv', subgroup=subgroup_name)
+                pooled_signal = pool_layer(signal_on_group)
+                self.assertEqual(pooled_signal.shape, self.pooled_shape[:-1] + (subgroup.order, self.shape[-1]))
+
+    def test_pool_equiv(self):
+        for group in self.group_dict.values():
+            signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+            pool_layer = self.pool(group=group, pool_size=2, strides=2, padding='same_equiv')
+            equiv_diff = test_equivariance(
+                pool_layer, signal_on_group, group_axis=self.group_axis, spatial_axes=self.spatial_axes)
+
+            self.assertAllLess(equiv_diff, 1e-4)
+
+    def test_pool_equiv_subgroup(self):
+        for group in self.group_dict.values():
+            for subgroup_name in group.subgroup.keys():
+                signal_on_group = tf.random.normal(shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42)
+                pool_layer = self.pool(
+                    group=group, pool_size=2, strides=2, padding='same_equiv', subgroup=subgroup_name)
+                equiv_diff = test_equivariance(
+                    pool_layer, signal_on_group, group_axis=self.group_axis, subgroup=subgroup_name,
+                    spatial_axes=self.spatial_axes)
+
+                self.assertAllLess(equiv_diff, 1e-4)
+
 
 tf.test.main()
