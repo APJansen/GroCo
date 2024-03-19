c81a9da27afdc62f7f2abf53c49ac1e3211fd86e
Aron <aronpjansen@gmail.com>
2024-03-19 18:03:53 +0100

    Join pooling tests

--- tests/test_pooling.py
+++ tests/test_pooling.py
@@ -7,15 +7,33 @@ from groco.utils import check_equivariance
 from tests.custom_testcase import KerasTestCase as TestCase
 
 
-class TestGroupPooling2D(TestCase):
+def get_attributes(dimension: int):
+    if dimension == 2:
+        pool_layer = GroupMaxPooling2D
+        group_dict = wallpaper_group_dict
+    elif dimension == 3:
+        pool_layer = GroupMaxPooling3D
+        group_dict = space_group_dict
+
+    return pool_layer, group_dict
+
+
+class TestGroupPoolingBase:
+    dimension = None
+
     def __init__(self, tests):
         super().__init__(tests)
-        self.shape = (9, 8, 8, 3)
-        self.pooled_shape = (9, 4, 4, 3)
-        self.spatial_axes = (1, 2)
-        self.group_axis = 3
-        self.pool = GroupMaxPooling2D
-        self.group_dict = wallpaper_group_dict
+        self.xsize = 8
+        self.batch_size = 2
+        self.input_features = 3
+        self.shape = (self.batch_size,) + (self.xsize,) * self.dimension + (self.input_features,)
+        self.pooled_shape = (
+            (self.batch_size,) + (self.xsize // 2,) * self.dimension + (self.input_features,)
+        )
+        self.spatial_axes = tuple(range(1, self.dimension + 1))
+        self.group_axis = self.dimension + 1
+
+        self.pool, self.group_dict = get_attributes(self.dimension)
 
     def test_pool_shape(self):
         for group in self.group_dict.values():
@@ -87,81 +105,9 @@ class TestGroupPooling2D(TestCase):
                 self.assertAllLess(equiv_diff, 1e-4)
 
 
-class TestGroupPooling3D(TestCase):
-    def __init__(self, tests):
-        super().__init__(tests)
-        self.shape = (9, 8, 8, 8, 3)
-        self.pooled_shape = (9, 4, 4, 4, 3)
-        self.spatial_axes = (1, 2, 3)
-        self.group_axis = 4
-        self.pool = GroupMaxPooling3D
-        self.group_dict = space_group_dict
+class TestGroupPooling2D(TestGroupPoolingBase, TestCase):
+    dimension = 2
 
-    def test_pool_shape(self):
-        for group in self.group_dict.values():
-            signal_on_group = keras.random.normal(
-                shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42
-            )
-            pool_layer = self.pool(group=group, pool_size=2, strides=2, padding="same_equiv")
-            pooled_signal = pool_layer(signal_on_group)
-            self.assertEqual(
-                pooled_signal.shape, self.pooled_shape[:-1] + (group.order, self.shape[-1])
-            )
 
-    def test_pool_shape_subgroup(self):
-        for group in self.group_dict.values():
-            for subgroup_name in group.subgroup.keys():
-                subgroup = self.group_dict[subgroup_name]
-                signal_on_group = keras.random.normal(
-                    shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42
-                )
-                pool_layer = self.pool(
-                    group=group,
-                    pool_size=2,
-                    strides=2,
-                    padding="same_equiv",
-                    subgroup=subgroup_name,
-                )
-                pooled_signal = pool_layer(signal_on_group)
-                self.assertEqual(
-                    pooled_signal.shape, self.pooled_shape[:-1] + (subgroup.order, self.shape[-1])
-                )
-
-    def test_pool_equiv(self):
-        for group in self.group_dict.values():
-            signal_on_group = keras.random.normal(
-                shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42
-            )
-            pool_layer = self.pool(group=group, pool_size=2, strides=2, padding="same_equiv")
-            equiv_diff = check_equivariance(
-                pool_layer,
-                signal_on_group,
-                group_axis=self.group_axis,
-                spatial_axes=self.spatial_axes,
-            )
-
-            self.assertAllLess(equiv_diff, 1e-4)
-
-    def test_pool_equiv_subgroup(self):
-        for group in self.group_dict.values():
-            for subgroup_name in group.subgroup.keys():
-                signal_on_group = keras.random.normal(
-                    shape=self.shape[:-1] + (group.order, self.shape[-1]), seed=42
-                )
-                pool_layer = self.pool(
-                    group=group,
-                    pool_size=2,
-                    strides=2,
-                    padding="same_equiv",
-                    subgroup=subgroup_name,
-                )
-                equiv_diff = check_equivariance(
-                    pool_layer,
-                    signal_on_group,
-                    group_axis=self.group_axis,
-                    acting_group=subgroup_name,
-                    spatial_axes=self.spatial_axes,
-                    target_group=subgroup_name,
-                )
-
-                self.assertAllLess(equiv_diff, 1e-4)
+class TestGroupPooling3D(TestGroupPoolingBase, TestCase):
+    dimension = 3
